e5599b031bd681e35fa8e467400c827ed8031622
mochuan <x001005xh@outlook.com>
2021-04-22 22:25:23 +0800

    small changes on chatbot ui

--- app/src/main/java/com/mdp/fypapp/Http/BotInfo.java
+++ app/src/main/java/com/mdp/fypapp/Http/BotInfo.java
@@ -1,7 +1,7 @@
 package com.mdp.fypapp.Http;
 
 public class BotInfo {
-    public static final String url = "http://150.158.192.203/:8000" ;// server site http://10.0.2.2:8000
+    public static final String url = "http://150.158.192.203:8000" ;// server site http://10.0.2.2:8000
     public static final String bot_key = "admin";
     public static final String[] action_type = {"talking", "training"};
 }
